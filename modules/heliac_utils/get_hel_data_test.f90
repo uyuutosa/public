6136577e98ca8aee72021b78e2deb9b6c716f855
Yu Sato <yu-sato819@i.softbank.jp>
2014-04-02 21:51:01 +0900

    delete test
    delete test

--- modules/heliac_utils/get_hel_data_test.f90
+++
@@ -1,7 +0,0 @@
-program main
-    use tmp
-    double precision, pointer :: x(:) => null(), y(:) => null()
-    call get_hel_data(80000,1, x, y)
-    print *,"aaa"
-    print *,x,y
-end program
--- modules/heliac_utils/test6.py
+++
@@ -1,4 +0,0 @@
-from mymodules import *
-a=fish()
-a.dset_wfcxx(80000,32)
-a.view()
