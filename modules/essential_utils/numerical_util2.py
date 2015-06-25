738721f5db1ef9dadbf104aee79ef7231eccd53e
Yu Sato <yu-sato819@i.softbank.jp>
2015-06-25 21:05:52 +0900

    Update numerical_util2.py

--- modules/essential_utils/numerical_util2.py
+++ modules/essential_utils/numerical_util2.py
@@ -24,3 +24,57 @@ def permutation_num(arr, idxarr=None):
         raise ValueError, "array is not invalid."
     plus = c[where(c > 0)]
     minus = c[where(c < 0)] * -1
+    #print c
+    return plus.size if plus.size > minus.size else minus.size
+
+def trapezoid(x,y):
+    dx = x[1] - x[0]
+    return sum(dx * (y[1:] + y[:-1]) / 2)
+
+
+
+def ascale(arr, margin=0.1):
+    #autoscale
+    r = 1. / arr.ptp()
+    retmin = (r * arr.min() - margin) / r
+    retmax = (r * arr.max() + margin) / r
+    return retmin, retmax
+
+
+def dec2bin(val, precision=10):
+    """Convert decimal number to binary number.
+    """
+    n = 1
+    vallst = str(val).split(".")
+    if len(vallst) == 2:
+        ret = "%s." %bin(int(vallst[0]))
+        x = float(vallst[1]) / (10 ** int(len(vallst[1])))
+        for i in xrange(precision):
+            if 2 * x < 1: 
+                ret += "0"
+                x *= 2
+            elif 2 * x > 1: 
+                ret += "1"
+                x = 2 * x - 1
+            else:
+                ret += "1"
+                break
+    else:
+        ret =  bin(int(vallst[0]))
+        
+     
+    return ret
+        
+        
+        
+def prime(N):
+    from numpy import arange, where
+    a = arange(2, N+1)
+    for i in xrange(2, N+1): a = a[where((a <= i) + (a%i != 0))]
+    return a
+
+
+
+def roll_str(line, N):
+   return line[-N:] + line[:-N]   
+
